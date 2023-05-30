b4a1a8f39c730ab4767ef54ed56979db3720ad0d
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-05-30 14:16:51 -0500

    Add city searching

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,14 +1,26 @@
 "use strict";
 
 const url = 'https://api.andrespecht.dev/movies';
+const cityUrl = 'https://countriesnow.space/api/v0.1/countries/cities';
 
 const options = {
     method: 'GET',
-    morde: 'cors'
+    mode: 'cors'
+};
+
+const cityOptions = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json; charset=utf-8'
+    },
+    body: JSON.stringify({
+        country: 'canada'
+    })
 };
 
 // global movies
 let movies = [];
+let cities = [];
 
 async function getMovies() {
     try {
@@ -82,13 +94,59 @@ function movieSearch() {
                 const div = document.createElement('div');
                 div.textContent = movie.title;
                 suggestions.appendChild(div);
-                console.log(movie.title);
+                // console.log(movie.title);
             });
         }
 
     });
     
-    // clear on esc
+    clearSearchOnEsc(searchInput, suggestions);
+}
+
+async function getCities() {
+    try {
+        const response = await fetch(cityUrl, cityOptions);
+        if(!response.ok) {
+            throw new Error('Network response error');
+        }
+
+        const data = await response.json();
+
+        cities = data.data;
+
+        // console.log(cities);
+
+        return data.response;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function searchCities() {
+    const searchInput = document.querySelector('.citysearch');
+    const suggestions = document.querySelector('.city-suggestions');
+
+    searchInput.addEventListener('input', (event) => {
+        const searchTerm = searchInput.value.toLowerCase();
+        suggestions.innerHTML = '';
+        // console.log(cities);
+        if (searchTerm.trim() !== ''){
+            const filteredCities = cities.filter(city =>
+                city.toLowerCase().includes(searchTerm)
+            );
+
+            filteredCities.forEach(city => {
+                const div = document.createElement('div');
+                div.textContent = city;
+                suggestions.appendChild(div);
+            });
+        }
+    });
+
+    clearSearchOnEsc(searchInput, suggestions);
+}
+
+function clearSearchOnEsc(searchInput, suggestions) {
     document.addEventListener('keydown', (event) => {
         if (event.key === 'Escape') {
             searchInput.value = '';
@@ -99,3 +157,5 @@ function movieSearch() {
 }
 
 movieSearch();
+getCities();
+searchCities();
