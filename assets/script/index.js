016d560c92ea013f51f9a2ce78d002fcb4d1645c
Apricosma <113317992+Apricosma@users.noreply.github.com>
2023-05-30 12:29:30 -0500

    return movies to dom

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,7 +1,61 @@
 "use strict";
 
-const array = [1, 2, 3, 4];
+const url = 'https://api.andrespecht.dev/movies';
 
-array.forEach((num) =>{
-    console.log(num);
-})
+const options = {
+    method: 'GET',
+    morde: 'cors'
+};
+
+async function getMovies() {
+    try {
+        const response = await fetch(url, options);
+        if(!response.ok) {
+            throw new Error('Network response error');
+        }
+
+        const data = await response.json();
+
+        console.log(data.response);
+
+        return data.response;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function printMovies(movies) {
+    movies.forEach(movie => {
+        const card = createMovieCard(movie);
+        appendCardToContainer(card);
+    })
+}
+
+function createMovieCard(movie) {
+    const card = document.createElement('div');
+    card.classList.add('moviecard');
+
+    const title = document.createElement('h2');
+    title.textContent = movie.title;
+
+    const year = document.createElement('p');
+    year.textContent = movie.year;
+
+    const img = document.createElement('img');
+    img.src = movie.poster;
+    img.alt = `Movie poster for ${movie.title}`;
+    
+    card.appendChild(img);
+    card.appendChild(title);
+    card.appendChild(year);
+
+    return card;
+}
+
+function appendCardToContainer(card) {
+    const container = document.querySelector('.movielist');
+    container.appendChild(card);
+}
+
+getMovies()
+    .then(movies => printMovies(movies));
